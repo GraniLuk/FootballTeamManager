bee39b1ccf942dd0988f15f61e49d0b8ac11da65
DFC\lgranica <granica.lukasz@gmail.com>
2017-08-28 07:30:19 +0200

    RouteConfig ustawiony na fixtures

--- FootballTeamManager/App_Start/RouteConfig.cs
+++ FootballTeamManager/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FootballTeamManager
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Fixture", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
