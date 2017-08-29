793ce0f1aa10bf42573878e862f71547b589e3a3
DFC\lgranica <granica.lukasz@gmail.com>
2017-08-29 07:35:07 +0200

    Wykomentowałem migrację z drużynami z ostatniego spotkania, wrzucę to ręcznie.

--- FootballTeamManager/Migrations/201708251757383_ZespolyOstatnieSpotkanie.cs
+++ FootballTeamManager/Migrations/201708251757383_ZespolyOstatnieSpotkanie.cs
@@ -7,20 +7,20 @@ namespace FootballTeamManager.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (4,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (7,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (32,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (35,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (39,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (53,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (54,1)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (8,2)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (36,2)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (37,2)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (38,2)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (40,2)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (47,2)");
-            Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (55,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (4,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (7,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (32,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (35,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (39,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (53,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (54,1)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (8,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (36,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (37,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (38,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (40,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (47,2)");
+            //Sql("INSERT INTO TeamPlayerAssociations (Player_Id,Team_Id) VALUES (55,2)");
         }
         
         public override void Down()
