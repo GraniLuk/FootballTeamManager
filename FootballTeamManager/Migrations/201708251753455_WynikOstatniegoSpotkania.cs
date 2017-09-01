65d9a86ac52d18080bba86d79371ed80f1385ab1
DFC\lgranica <granica.lukasz@gmail.com>
2017-09-01 07:06:35 +0200

    Usunąłem wszystkie inserty z migracji.

--- FootballTeamManager/Migrations/201708251753455_WynikOstatniegoSpotkania.cs
+++ FootballTeamManager/Migrations/201708251753455_WynikOstatniegoSpotkania.cs
@@ -7,9 +7,9 @@ namespace FootballTeamManager.Migrations
     {
         public override void Up()
         {
-            Sql("Insert into Teams ([Name]) VALUES('Cwaniaki')");
-            Sql("Insert into Teams ([Name]) VALUES('Cieniasy')");
-            Sql("Insert into Fixtures (Date,[FirstTeamScore],[SecondTeamScore],[FirstTeam_Id],[SecondTeam_Id]) VALUES('2017-08-22',9,8,1,2) ");
+            //Sql("Insert into Teams ([Name]) VALUES('Cwaniaki')");
+            //Sql("Insert into Teams ([Name]) VALUES('Cieniasy')");
+            //Sql("Insert into Fixtures (Date,[FirstTeamScore],[SecondTeamScore],[FirstTeam_Id],[SecondTeam_Id]) VALUES('2017-08-22',9,8,1,2) ");
            
         }
         
