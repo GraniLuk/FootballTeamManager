f8268bc90f1d5c2fd0149804c9371826de0256c6
DFC\lgranica <granica.lukasz@gmail.com>
2017-09-05 09:27:26 +0200

    Poprawka do zapisywania piłkarza

--- FootballTeamManager/Controllers/PlayersController.cs
+++ FootballTeamManager/Controllers/PlayersController.cs
@@ -59,7 +59,7 @@ namespace FootballTeamManager.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Rank,Active")] Player player)
+        public ActionResult Create([Bind(Include = "Id,Name,Skill,Active")] Player player)
         {
             if (ModelState.IsValid)
             {
@@ -91,7 +91,7 @@ namespace FootballTeamManager.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Rank,Active")] Player player)
+        public ActionResult Edit([Bind(Include = "Id,Name,Skill,Active")] Player player)
         {
             if (ModelState.IsValid)
             {
