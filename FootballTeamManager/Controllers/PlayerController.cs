07a0fc041283c0099d125ff4b4574ab179feb553
granica.lukasz <granica.lukasz@live.com>
2017-09-11 19:18:59 +0200

    Poprawka do edycji piłkarza

--- FootballTeamManager/Controllers/PlayerController.cs
+++ FootballTeamManager/Controllers/PlayerController.cs
@@ -113,7 +113,7 @@ namespace FootballTeamManager.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Skill,Active")] Player player)
+        public ActionResult Edit([Bind(Include = "Id,Name,Skill,Active,ShortName")] Player player)
         {
             if (ModelState.IsValid)
             {
