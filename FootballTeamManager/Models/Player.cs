0767db71f5671fb7c42e1cf3b21bda8b1c5404b8
granica.lukasz <granica.lukasz@live.com>
2017-09-17 11:46:44 +0200

    Dodanie spisu graczy do paska nawigacyjnego

--- FootballTeamManager/Models/Player.cs
+++ FootballTeamManager/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
@@ -14,6 +15,7 @@ namespace FootballTeamManager.Models
         }
         public int Id { get; set; }
         [Required]
+        [DisplayName("Piłkarz")]
         [StringLength(30)]
         public string Name { get; set; }
         [Required]
