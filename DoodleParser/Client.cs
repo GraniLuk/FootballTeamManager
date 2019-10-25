2ad1a56ce23a5a12aa0b929e6dbaf4646d76a968
granica.lukasz <granica.lukasz@live.com>
2019-10-25 20:23:28 +0200

    Add GetParticipants method with unit tests.

---
+++ DoodleParser/Client.cs
@@ -0,0 +1,22 @@
+﻿using FootballTeamManagerTests.DoodleApi;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace DoodleParser
+{
+    public class Client
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public List<Participant> GetParticipants(string uri)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Uri url = new Uri(uri);
+            var responseString = client.GetStringAsync(uri).Result;
+            return JsonConvert.DeserializeObject<RootObject>(responseString).participants;
+        }
+    }
+}
---
+++ DoodleParserTests/GetParticipantsTests.cs
@@ -0,0 +1,32 @@
+﻿using DoodleParser;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoodleParserTests
+{
+    [TestFixture]
+    public class ParsingDoodleTests
+    {
+        [Test]
+        public void GetAllParticipants()
+        {
+            var response = new Client().GetParticipants("http://doodle.com/api/v2.0/polls/vxmqh3weap4az3cu/").Count;
+
+            Assert.AreEqual(20, response);
+
+        }
+
+        [Test]
+        public void GetAllActiveParticipantsForLastWeek()
+        {
+            var response = new Client().GetParticipants("http://doodle.com/api/v2.0/polls/vxmqh3weap4az3cu/").Count(x => x.preferences.LastOrDefault() == 1);
+
+            Assert.AreEqual(12, response);
+        }
+
+    }
+}
