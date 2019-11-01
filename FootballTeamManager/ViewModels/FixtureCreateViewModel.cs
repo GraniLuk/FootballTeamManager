82099ffa3388e8c48faca1c32626b95d3548a83b
granica.lukasz <granica.lukasz@live.com>
2019-11-01 20:04:21 +0100

    Add new viewmodel to create new fixture.

---
+++ FootballTeamManager/ViewModels/FixtureCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FootballTeamManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballTeamManager.ViewModels
+{
+    public class FixtureCreateViewModel
+    {
+        public DateTime Date { get; set; }
+        public List<Team> TeamsAToChoose { get; set; }
+        public List<Team> TeamsBToChoose { get; set; }
+    }
+}
