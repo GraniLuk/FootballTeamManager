b06db722383bb31f93576b690cd1239ea84c3e11
granica.lukasz <granica.lukasz@live.com>
2019-12-01 17:29:23 +0100

    Start implementation of GetSkillService.

---
+++ FootballTeamManager/Skills/Infrastucture/FixturesRepository.cs
@@ -0,0 +1,28 @@
+﻿using FootballTeamManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FootballTeamManager.Skills.Infrastucture
+{
+    public class FixturesRepository : IFixturesRepository
+    {
+        public Task<Fixture> GetForATeam(int teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fixture> GetForBTeam(int teamId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IFixturesRepository
+    {
+        Task<Fixture> GetForATeam(int teamId);
+        Task<Fixture> GetForBTeam(int teamId);
+    }
+}
