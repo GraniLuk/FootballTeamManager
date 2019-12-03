10da6b5a3bf978b2ab389f3ac30432e6aca5339f
granica.lukasz <granica.lukasz@live.com>
2019-12-03 20:01:46 +0100

    The same in playersTeam repository, cache all entity at the first.

--- FootballTeamManager/Skills/Infrastucture/PlayersTeamRepository.cs
+++ FootballTeamManager/Skills/Infrastucture/PlayersTeamRepository.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
+using System;
+
 namespace FootballTeamManager.Skills.Infrastucture
 {
     internal class PlayersTeamRepository : IPlayersTeamRepository
     {
         private readonly ApplicationDbContext _context;
+        IList<TeamPlayerAssociation> _allTeamPlayerAssociatons;
         public PlayersTeamRepository()
         {
             _context = new ApplicationDbContext();
+            _allTeamPlayerAssociatons = GetAll();
+        }
+
+        private IList<TeamPlayerAssociation> GetAll()
+        {
+            return _context.TeamPlayerAssociations.Include(x => x.Team).ToList();
         }
 
         public IList<TeamPlayerAssociation> GetAllTeamsForPlayer(int playerId)
         {
-            return _context.TeamPlayerAssociations.Include(x=>x.Team)
+            return _allTeamPlayerAssociatons
                 .Where(x => x.Player.Id == playerId).ToList();
         }
     }
