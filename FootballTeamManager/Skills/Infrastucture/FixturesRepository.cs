21bbde107721d7df3a8896799d08984fad045753
granica.lukasz <granica.lukasz@live.com>
2019-12-03 19:59:57 +0100

    Get all fixtures at the first and then use List in all other methods.

--- FootballTeamManager/Skills/Infrastucture/FixturesRepository.cs
+++ FootballTeamManager/Skills/Infrastucture/FixturesRepository.cs
@@ -11,22 +11,29 @@ namespace FootballTeamManager.Skills.Infrastucture
     public class FixturesRepository : IFixturesRepository
     {
         private readonly ApplicationDbContext _context;
+        private readonly List<Fixture> _allFixtures;
         public FixturesRepository()
         {
             _context = new ApplicationDbContext();
+            _allFixtures = GetAllFixtures();
         }
         public Fixture GetForATeam(int teamId)
         {
 
-                return _context.Fixtures.FirstOrDefault(i => i.FirstTeam.Id == teamId);
+                return _allFixtures.FirstOrDefault(i => i.FirstTeam.Id == teamId);
             
         }
 
         public Fixture GetForBTeam(int teamId)
         {
             
-                return _context.Fixtures.FirstOrDefault(i => i.SecondTeam.Id == teamId);
+                return _allFixtures.FirstOrDefault(i => i.SecondTeam.Id == teamId);
             
+        }
+
+        private List<Fixture> GetAllFixtures()
+        {
+            return _context.Fixtures.ToList();
         }
     }
 
