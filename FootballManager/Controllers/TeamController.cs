e2771c965d7eb338796cd4cbdac771225407de75
granica.lukasz <granica.lukasz@live.com>
2020-02-22 19:59:21 +0100

    Add team controller.

---
+++ FootballManager/Controllers/TeamController.cs
@@ -0,0 +1,50 @@
+﻿using FootballManager.Data;
+using FootballManager.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FootballManager.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TeamController(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new BadRequestResult();
+            }
+            var exam = _db.Players.ToList();
+
+            var teamDisplayViewModel = new TeamDisplayViewModel()
+            {
+                TeamId = id.Value,
+                Players = (from player in _db.Players
+                           join teamPlayerAssociation in _db.TeamPlayerAssociations on player.Id equals teamPlayerAssociation.Player.Id
+                           where teamPlayerAssociation.Team.Id == id.Value
+                           select player).ToList()
+            };
+
+            return View(teamDisplayViewModel);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
