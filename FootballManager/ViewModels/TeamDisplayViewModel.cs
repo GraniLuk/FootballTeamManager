301c04d552d3b3f10a07c9110b2b57179efd8fd3
granica.lukasz <granica.lukasz@live.com>
2020-02-22 19:59:21 +0100

    Add team controller.

---
+++ FootballManager/ViewModels/TeamDisplayViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FootballManager.Models;
+using System.Collections.Generic;
+
+namespace FootballManager.ViewModels
+{
+    public class TeamDisplayViewModel
+    {
+        public int TeamId { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
