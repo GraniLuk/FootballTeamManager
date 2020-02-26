cd9fbcb95100536f02dfaa020c60342ae4cc3f54
granica.lukasz <granica.lukasz@live.com>
2020-02-26 19:04:48 +0100

    Add DoodleParser

---
+++ DoodleParser/Client/ParticipantsService.cs
@@ -0,0 +1,27 @@
+﻿using DoodleApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoodleApi.Client
+{
+    public class ParticipantsService
+    {
+        private readonly RootObject _rootObject;
+        public ParticipantsService(RootObject rootObject)
+        {
+            _rootObject = rootObject;
+        }
+        private List<Participant> GetParticipants()
+        {
+            return _rootObject.participants;
+        }
+
+        public List<Participant> GetActiveParticipantsAt(DateTime date)
+        {
+            var option = _rootObject.CheckPositionDateInArray(date);
+            return GetParticipants().Where(x => x.preferences[option] == 1).ToList();
+        }
+    }
+}
---
+++ DoodleParser/DoodleApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using DoodleApi.Model;
+using DoodleApi.Client;
+
+namespace DoodleApi
+{
+    public class DoodleApi : IApi
+    {
+
+        private readonly string DoodleUrl;
+        public DoodleApi(string doodleUrl)
+        {
+            DoodleUrl = doodleUrl;
+        }
+
+        public List<Participant> GetActivePlayersAt(DateTime date)
+        {
+            var rootObjectClient = new RootObjectClient(DoodleUrl);
+            var rootObject = rootObjectClient.GetRootObject();
+            var participantsService = new ParticipantsService(rootObject);
+            return participantsService.GetActiveParticipantsAt(date);
+        }
+
+    }
+}
