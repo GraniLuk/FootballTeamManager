55b057458d7774327223b9d5fab4e1283e1499f7
Łukasz Mateusz Granica <lukasz.granica@banqsoft.pl>
2020-03-08 14:58:43 +0100

    Remove Ranking controller unused methods.

--- FootballManager/Controllers/RankingController.cs
+++ FootballManager/Controllers/RankingController.cs
@@ -1,16 +1,15 @@
 ﻿using FootballManager.Data;
 using FootballManager.ViewModels;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 
-namespace FootballTeamManager.Controllers
-{
+namespace FootballTeamManager.Controllers {
     public class RankingController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public RankingController(ApplicationDbContext context)
         {
             _db = context;
@@ -28,77 +27,6 @@ namespace FootballTeamManager.Controllers
             return View(ranking);
         }
 
-        // GET: Ranking/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Ranking/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Ranking/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Ranking/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Ranking/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Ranking/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Ranking/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
