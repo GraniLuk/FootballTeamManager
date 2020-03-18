7f329fd8d78d0e7bdf997eee531bc3657cea9ab7
Łukasz Mateusz Granica <lukasz.granica@banqsoft.pl>
2020-03-18 07:24:17 +0100

    First test using in memory db.

--- FootballManagerTests/DoodleRefreshTests.cs
+++ FootballManagerTests/DoodleRefreshTests.cs
@@ -1,9 +1,13 @@
 ﻿using DoodleApi;
+using DoodleApi.Model;
 using FootballManager.Controllers;
 using FootballManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FootballManagerTests {
     [TestFixture]
@@ -11,12 +15,26 @@ namespace FootballManagerTests {
         [Test]
         public void DoodleRefresh_UpdatesActiveOnPlayer() {
             var doodleApiMoq = new Mock<IApi>();
+            doodleApiMoq.Setup(x => x.GetActivePlayersAt(It.IsAny<DateTime>())).Returns(new List<Participant> { new Participant {
+                id = 1,
+                name = "LukaszG"
+            }
+            });
             var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Add_writes_to_database")
                 .Options;
 
-            var playerController = new PlayerController(doodleApiMoq.Object, new ApplicationDbContext(dbOptions));
+            using (var context = new ApplicationDbContext(dbOptions)) {
+                context.Players.Add(new FootballManager.Models.Player { DoodleName = "LukaszG", Active = false });
+                context.SaveChanges();
 
-            //playerController.DoodleRefresh(DateTime.UtcNow);
+                var playerController = new PlayerController(doodleApiMoq.Object, context);
+
+                playerController.DoodleRefresh(DateTime.UtcNow);
+
+                Assert.IsTrue(context.Players.Single().Active);
+            }
+
+
 
         }
     }
