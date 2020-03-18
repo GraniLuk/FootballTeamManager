5d49a4d538e214c069d819ea8d8ba88fa7854799
Łukasz Mateusz Granica <lukasz.granica@banqsoft.pl>
2020-03-18 07:09:01 +0100

    Use InMemoryDatabase in tests.

---
+++ FootballManagerTests/DoodleRefreshTests.cs
@@ -0,0 +1,23 @@
+﻿using DoodleApi;
+using FootballManager.Controllers;
+using FootballManager.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace FootballManagerTests {
+    [TestFixture]
+    public class DoodleRefreshTests {
+        [Test]
+        public void DoodleRefresh_UpdatesActiveOnPlayer() {
+            var doodleApiMoq = new Mock<IApi>();
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+
+            var playerController = new PlayerController(doodleApiMoq.Object, new ApplicationDbContext(dbOptions));
+
+            //playerController.DoodleRefresh(DateTime.UtcNow);
+
+        }
+    }
+}
